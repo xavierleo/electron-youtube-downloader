27dfb7a644247d5883bcf87d4d75341143738bc0
Xavier Lawrence <xleo.lawrence@gmail.com>
2026-05-01 19:01:17 +0200

    chore: upgrade electron app dependencies

--- main.js
+++ main.js
@@ -13,7 +13,14 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      contextIsolation: false,
+      nodeIntegration: true
+    }
+  })
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
--- renderer.js
+++ renderer.js
@@ -4,9 +4,7 @@
 'use strict';
 
 const path = require('path');
-const util = require('util');
-const fs = require('fs');
-const youtubedl = require('youtube-dl');
+const youtubedl = require('youtube-dl-exec');
 
 // Optional arguments passed to youtube-dl.
 
@@ -22,119 +20,60 @@ $('#search-details').on('click', '.download-video', function(event){
     // alert(url);
     downloadVideo(url);
 });
-function getInfo(infoUrl){
+async function getInfo(infoUrl){
     let url = infoUrl;
     let html = '';
-    youtubedl.getInfo(url, function(err, info) {
-        if (err) throw err;
-        
-        if(util.isArray(info)){
-            info.forEach(function(element) {
-                let downloadURL = "https://www.youtube.com/watch?v=";
-                html += '<div class="row">';
-                html +=     '<div class="col-xs-12">';
-                html +=         '<h3>' + element.title + '</h3>';
-                html +=     '</div>';
-                html +=     '<div class="col-xs-3">';
-                html +=         '<img src="'+ element.thumbnail +'" class="img-responsive"/>';
-                html +=     '</div>';
-                html +=     '<div class="col-xs-9">';
-                html +=         '<h5>' + element._filename + '</h5>';
-                html +=         '<button class="btn btn-default download-video" data-url="'+downloadURL.concat(info.id)+'" type="submit">Download</button><p class="precentage"></p>';
-                html +=     '</div>';
-                html += '</div>';
-                // console.log('id:', element.id);
-                // console.log('title:', element.title);
-                // console.log('url:', element.url);
-                // console.log('thumbnail:', element.thumbnail);
-                // console.log('description:', element.description);
-                // console.log('filename:', element._filename);
-                // console.log('format id:', element.format_id);
-            }, this);
-            
-        }else {
-            let downloadURL = "https://www.youtube.com/watch?v=";
+    try {
+        let info = await youtubedl(url, {
+            dumpSingleJson: true,
+            noWarnings: true,
+            noCheckCertificates: true
+        });
+        let entries = Array.isArray(info.entries) ? info.entries : [info];
+
+        entries.forEach(function(element) {
+            let downloadURL = element.webpage_url || element.original_url || element.url || url;
             html += '<div class="row">';
             html +=     '<div class="col-xs-12">';
-            html +=         '<h3>' + info.title + '</h3>';
+            html +=         '<h3>' + element.title + '</h3>';
             html +=     '</div>';
             html +=     '<div class="col-xs-3">';
-            html +=         '<img src="'+ info.thumbnail +'" class="img-responsive"/>';
+            html +=         '<img src="'+ element.thumbnail +'" class="img-responsive"/>';
             html +=     '</div>';
             html +=     '<div class="col-xs-9">';
-            html +=         '<h5>' + info._filename + '</h5>';
-            html +=         '<button class="btn btn-default download-video" data-url="'+downloadURL.concat(info.id)+'" type="submit">Download</button><p class="precentage"></p>';
+            html +=         '<h5>' + (element.filename || element.fulltitle || element.title) + '</h5>';
+            html +=         '<button class="btn btn-default download-video" data-url="'+downloadURL+'" type="submit">Download</button><p class="precentage"></p>';
             html +=     '</div>';
             html += '</div>';
-
-            console.log('id:', info.id);
-            console.log('title:', info.title);
-            console.log('url:', info.url);
-            // console.log('thumbnail:', info.thumbnail);
-            // console.log('description:', info.description);
-            // console.log('filename:', info._filename);
-            // console.log('format id:', info.format_id);
-            
-        }
+        }, this);
         $('#search-details').append(html);
-    });
+    } catch (err) {
+        console.error(err);
+        $('#search-details').append('<p class="text-danger">Unable to fetch video details.</p>');
+    }
 }
 function downloadVideo(url){
-    let video = youtubedl(url,
-    // Optional arguments passed to youtube-dl.
-    ['--format=18'],
-    // Additional options can be given for calling `child_process.execFile()`.
-    { cwd: __dirname });
+    let video = youtubedl.exec(url, {
+        format: 18,
+        output: path.join(__dirname, '%(title)s.%(ext)s'),
+        newline: true
+    }, { cwd: __dirname });
 
-    // Will be called when the download starts.
-    let size = 0
-    video.on('info', function(info) {
-        size = info.size;
-        console.log('Download started');
-        console.log('filename: ' + info._filename);
-        console.log('size: ' + info.size);
-        let output = path.join(__dirname + '/', info._filename + '.mp4');
-        video.pipe(fs.createWriteStream(output));
+    video.stderr.on('data', function data(chunk) {
+        let output = chunk.toString();
+        let match = output.match(/\[download\]\s+(\d+(?:\.\d+)?)%/);
+        if (match) {
+            $('.precentage').text(match[1] + '%');
+        }
+        console.log(output);
     });
 
-    
-    let pos = 0;
-    video.on('data', function data(chunk) {
-        pos += chunk.length;
-        // `size` should not be 0 here.
-        if (size) {
-            let percent = (pos / size * 100).toFixed(2);
-            
-            console.log(percent + '%');
-            process.stdout.cursorTo(0);
-            process.stdout.clearLine(1);
-            process.stdout.write(percent + '%');
-        }
+    video.catch(function(err) {
+        console.error(err);
+        $('.precentage').text('Download failed');
     });
 
 }
 function downloadPlaylist(url){
-    let video = youtubedl(url);
-    video.on('error', function error(err) {
-        console.log('error 2:', err);
-    });
-    let size = 0;
-    video.on('info', function(info) {
-        size = info.size;
-        let output = path.join(__dirname + '/', info._filename + '.mp4');
-        video.pipe(fs.createWriteStream(output));
-    });
-    let pos = 0;
-    video.on('data', function data(chunk) {
-        pos += chunk.length;
-        // `size` should not be 0 here.
-        if (size) {
-            let percent = (pos / size * 100).toFixed(2);
-            process.stdout.cursorTo(0);
-            process.stdout.clearLine(1);
-            process.stdout.write(percent + '%');
-        }
-    });
-
-    video.on('next', downloadPlaylist);
+    downloadVideo(url);
 }
